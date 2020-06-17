4db9df99c54ee8ed696eabecc27a52193c123a2e
liulin520-y <1484992230@qq.com>
2020-06-17 10:31:43 +0800

    项目搭建

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="box">
+    <div class="main-content"></div>
+    <div class="switch-btn">
+        <div class="switch-btn-item"><img src="" alt="">1</div>
+        <div class="switch-btn-item"><img src="" alt="">2</div>
+        <div class="switch-btn-item"><img src="" alt="">6</div>
+        <div class="switch-btn-item"><img src="" alt="">5</div>
+        <div class="switch-btn-item"><img src="" alt="">7</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style>
+.box{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+.main-content{
+    flex-grow: 1;
+    background-color: aqua;
+}
+.switch-btn{
+    display: flex;
+    align-items: center;
+    text-align: center;
+    background-color: rgb(255, 0, 212);
+}
+.switch-btn .switch-btn-item{
+    width: 20%;
+    padding: 10px 0;
+
+}
+</style>
