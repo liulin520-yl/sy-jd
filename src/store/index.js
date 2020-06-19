5892d5244ed577543f9ab43ba5e0885a3a84b800
ancyBit <1472396126@qq.com>
2020-06-19 16:38:31 +0800

    汪永

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    landing:false
   },
   mutations: {
   },
--- src/views/No-Load.vue
+++ src/views/No-Load.vue
@@ -8,27 +8,62 @@
     </div>
     <div class="no-load-body-y">
       <!-- 手机账户 -->
-      <div class="no-load-tel">
+      <div class="no-load-tel" v-if="showthree">
         <div class="number-y">{{areanum}}</div>
         <div class="area-y">
           <img src="../assets/classify-img/bottom.png" alt />
         </div>
         <div class="input-y">
-          <input type="text" placeholder="请输入手机号"  @focus="phoneFocus" @blur="phoneblur" v-model="tel">
+          <input
+            type="text"
+            placeholder="请输入手机号"
+            @focus="phoneFocus"
+            @blur="phoneblur"
+            v-model="tel"
+          />
         </div>
         <div class="delete-y" @click="deleteone">
-          <img src="../assets/classify-img/cha.png" alt v-show="showone">
+          <img src="../assets/classify-img/cha.png" alt v-show="showone" />
         </div>
       </div>
+      <div class="no-load-id" v-else>
+          <div class="id-name-y">
+            <input type="text"  v-model="tel"  placeholder="用户名/邮箱/手机号"  @focus="phoneFocus"  @blur="phoneblur">
+          </div>
+          <div class="id-img-y"  @click="deleteone">
+              <img src="../assets/classify-img/cha.png" alt v-show="showone">
+          </div>
+      </div>
       <!-- 验证码 -->
-      <div class="password-y">
+      <div class="password-y" v-if="showfour">
         <div class="pass-number-y">
-          <input type="password" placeholder="请输入收到的验证码" @focus="passFocus" @blur="passblur" v-model="pass">
+          <input
+            type="password"
+            placeholder="请输入收到的验证码"
+            @focus="passFocus"
+            @blur="passblur"
+            v-model="pass"
+          />
         </div>
-        <div class="cha2-y"  @click="deletetwo">
-          <img src="../assets/classify-img/cha.png" v-show="showtwo">
+        <div class="cha2-y" @click="deletetwo">
+          <img src="../assets/classify-img/cha.png" v-show="showtwo" />
         </div>
-        <div class="verify" :class="{numcolor:see}">获取验证码</div>
+        <div class="verify" :class="{numcolor:see}" @click="btn">{{authCode}}</div>
+      </div>
+      <div class="id-password-y" v-else>
+            <div class="id-password-y-input">
+                 <input type="text"  placeholder="请输入密码" v-model="pass" @focus="passFocus"
+            @blur="passblur">
+            </div>
+            <div class="id-password-y-see">
+                  <img src="../assets/classify-img/hu.png" alt>
+            </div>
+            <div class="id-password-cha"  @click="deletetwo">
+                   <img src="../assets/classify-img/cha.png" alt v-show="showtwo">
+            </div>
+            <div class="wang">
+                 忘记密码
+            </div>
       </div>
       <!-- 登录 -->
       <div class="landing-y" :class="{active:judge}" @click="login">登录</div>
@@ -36,7 +71,7 @@
       <div class="landing-one-y">一键登录</div>
       <!-- 账户密码 -->
       <div class="id-password">
-        <div>账户密码登录</div>
+        <div @click="loginclick">{{loginway}}</div>
         <div>手机快速注册</div>
       </div>
       <!-- 其他登录方式 -->
@@ -47,88 +82,142 @@
       </div>
       <!-- 登录图标-->
       <div class="landing-img-box">
-         <div>
-           <img src="../assets/classify-img/qq.png" alt="">
-           <p>QQ</p>
-         </div>
-         <div>
-           <img src="../assets/classify-img/wx.png" alt="">
-           <p>微信</p>
-         </div>
-         <div>
-           <img src="../assets/classify-img/iphone.png" alt="">
-           <p>苹果</p>
-         </div>
+        <div>
+          <img src="../assets/classify-img/qq.png" alt />
+          <p>QQ</p>
+        </div>
+        <div>
+          <img src="../assets/classify-img/wx.png" alt />
+          <p>微信</p>
+        </div>
+        <div>
+          <img src="../assets/classify-img/iphone.png" alt />
+          <p>苹果</p>
+        </div>
       </div>
-      <p class="protocol">未注册的手机号验证后将自动创建京东账号, 
-        登录即代表您已同意<span>京东隐私政策</span></p>
+      <p class="protocol">
+        未注册的手机号验证后将自动创建京东账号,
+        登录即代表您已同意
+        <span>京东隐私政策</span>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-         see:false,
-         areanum:"+86",
-         showone:false,
-         showtwo:false,
-         tel:"",
-         pass:"",
-         judge:false
-    }
+  data() {
+    return {
+      see: false,
+      areanum: "+86",
+      showone: false,
+      showtwo: false,
+      showthree:true,
+      tel: "",
+      pass: "",
+      judge: false,
+      authCode: "获取验证码",
+      num: 60,
+      timer: null,
+      loginway:"账户密码登录",
+      showfour:true,
+      textname:"18655600250",
+      textpass:"0000"
+    };
   },
-  methods:{
-    phoneFocus(){
-      this.showone=true
+    computed: {
+        landing() {
+            return this.$store.state.landing
+        },
     },
-    passFocus(){
-      this.showtwo=true
+  methods: {
+    phoneFocus() {
+      this.showone = true;
     },
-    phoneblur(){
-      this.showone=false
+    passFocus() {
+      this.showtwo = true;
     },
-    passblur(){
-      this.showtwo=false;
+    phoneblur() {
+      this.showone = false;
     },
-    bgcevent(){
-      if(this.tel.length==11){
-        if(this.pass.length!=0){
-          this.judge=true
-        }else{
-          this.judge=false
+    passblur() {
+      this.showtwo = false;
+    },
+    bgcevent() {
+      if (this.tel.length == 11) {
+        if (this.pass.length != 0) {
+          this.judge = true;
+        } else {
+          this.judge = false;
         }
       }
     },
-    seem(){
-      if(this.tel.length==11){
-        this.see=true
-      }else{
-        this.see=false
+    seem() {
+      if (this.tel.length == 11) {
+        this.see = true;
+      } else {
+        this.see = false;
       }
     },
-    deleteone(){
-      this.tel=""
+    deleteone() {
+      this.tel = "";
     },
-    deletetwo(){
-      this.pass=""
+    deletetwo() {
+      this.pass = "";
     },
-    personReturn(){
-       this.$router.go(-1)
+    personReturn() {
+      this.$router.go(-1);
     },
-    login(){
-       this.$router.push("personage-page")
-    }
+    btn() {
+      if (this.tel.length == 11) {
+        let that = this;
+        this.timer = setInterval(function() {
+          that.num--;
+          that.authCode = "重新获取(" + that.num + "s)";
+        if (that.num == 0) {
+          clearInterval(that.timer);
+          that.authCode="获取验证码"
+          that.num =60
+        }
+        }, 1000);
+      }
+    },
+    loginclick(){
+      if(this.loginway=="账户密码登录"){
+        this.loginway="短信验证码登录"
+        this.showthree=!this.showthree
+        this.showfour=!this.showfour
+      }else{
+        this.loginway="账户密码登录"
+        this.showthree=!this.showthree
+        this.showfour=!this.showfour
+      }
+    },
+    // 登录
+    login() {
+      clearInterval(this.timer);
+      this.authCode="获取验证码"
+      this.num =60
+      if(this.tel==this.textname){
+        if(this.pass==this.textpass){
+          this.$router.push("personage-page");
+          this.$store.state.landing=true
+        }
+      }
+    },
   },
-  watch:{
-    tel:function(){
-        this.bgcevent(),
-        this.seem()
+  watch: {
+    tel: function() {
+      this.bgcevent(), this.seem();
+      if(this.tel.length!=11){
+         this.num=60
+         clearInterval(this.timer);
+         this.authCode="获取验证码"
+      }
     },
-    pass:function(){
-        this.bgcevent()
-    } 
+    pass: function() {
+      this.bgcevent();
+    }
   }
 };
 </script>
@@ -201,6 +290,7 @@ export default {
   height: 29px;
   text-align: center;
   line-height: 29px;
+  margin: 0px 5px;
 }
 .delete-y img {
   width: 14px;
@@ -230,6 +320,7 @@ export default {
   height: 29px;
   text-align: center;
   line-height: 29px;
+  margin: 0px 5px;
 }
 .cha2-y img {
   width: 14px;
@@ -243,7 +334,7 @@ export default {
   text-align: center;
   border-left: 1px solid #ccc;
 }
-.numcolor{
+.numcolor {
   color: #f10000;
 }
 /* 登录 */
@@ -258,7 +349,7 @@ export default {
   text-align: center;
   line-height: 50px;
 }
-.active{
+.active {
   background-color: rgb(255, 31, 0);
 }
 /* 一键登录 */
@@ -281,7 +372,7 @@ export default {
   flex-direction: row;
   justify-content: space-between;
   font-size: 14px;
-  color: rgb(0, 0,0, 0.4);
+  color: rgb(0, 0, 0, 0.4);
 }
 /* 登录方法 */
 .landing-way-y {
@@ -311,8 +402,9 @@ export default {
   height: 1px;
   background-color: rgb(0, 0, 0, 0.2);
 }
+
 /* 登录图标 */
-.landing-img-box{
+.landing-img-box {
   width: 100%;
   height: 69px;
   margin: 25px 0px 12px 0px;
@@ -320,23 +412,101 @@ export default {
   flex-direction: row;
   justify-content: center;
 }
-.landing-img-box>div{
+.landing-img-box > div {
   width: 48px;
   height: 69px;
   margin: 0px 15px;
   text-align: center;
   font-size: 12px;
 }
-.landing-img-box>div img{
+.landing-img-box > div img {
   width: 48px;
 }
-.protocol{
+.protocol {
   text-align: center;
   font-size: 13px;
-  color: rgb(0, 0, 0,0.3);
+  color: rgb(0, 0, 0, 0.3);
 }
-.protocol span{
-  color: #4A90E2;
+.protocol span {
+  color: #4a90e2;
+}
+/* 账号登录 */
+.no-load-id{
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid rgb(240, 240, 240);
+}
+.id-name-y{
+  flex-grow: 1;
+}
+.id-name-y input{
+  width: 100%;
+  height: 29px;
+  border: none;
+  outline: none;
+}
+.id-img-y{
+  width: 14px;
+  height: 29px;
+  text-align: center;
+  line-height: 29px;
+}
+.id-img-y img{
+  width: 14px;
+  height: 14px;
+}
+/* 密码 */
+.id-password-y{
+  margin-top: 20px;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid rgb(240, 240, 240);
+}
+
+.id-password-y-input{
+  flex-grow: 1;
+}
+.id-password-y-input input{
+  width: 100%;
+  height: 29px;
+  border: none;
+  outline: none;
+}
+.id-password-y-see{
+  width: 20px;
+  height: 29px;
+  text-align: center;
+  line-height: 29px;
+  margin: 0px 5px;
+}
+.id-password-y-see img{
+  width: 20px;
+  height: 20px;
+}
+.id-password-cha{
+  width: 14px;
+  height: 29px;
+  text-align: center;
+  line-height: 29px;
+  margin: 0px 5px;
+}
+.id-password-cha img{
+  width: 14px;
+  height: 14px;
+}
+.wang{
+  width: 110px;
+  height: 23px;
+  color: #848689;
+  font-size: 14px;
+  text-align: center;
+  border-left: 1px solid #ccc;
 }
 </style>>
 
