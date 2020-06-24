a5a2ea8f460c611e1bc5a026a20366c46cbe5c4a
liulin520-y <1484992230@qq.com>
2020-06-24 14:57:01 +0800

    ll

--- src/components/PartMain.vue
+++ src/components/PartMain.vue
@@ -356,7 +356,7 @@
     <!-- 底部 -->
     <van-goods-action>
       <van-goods-action-icon icon="chat-o" text="客服" dot />
-      <van-goods-action-icon icon="cart-o" text="购物车" badge="5" />
+      <van-goods-action-icon icon="cart-o" text="购物车" :badge="gwcList.count" />
       <van-goods-action-icon icon="shop-o" text="店铺" badge="12" />
       <van-goods-action-button type="warning" @click="addCratl(page)" text="加入购物车" />
       <van-goods-action-button type="danger" text="立即购买" />
@@ -383,6 +383,9 @@ export default {
   computed: {
     orderList() {
       return this.$store.state.orderList;
+    },
+    gwcList(){
+      return this.$store.state.gwcList;
     }
   },
   methods: {
@@ -437,7 +440,9 @@ export default {
         count: this.orderList[page].count,
         price: this.orderList[page].price,
         img1: this.orderList[page].img1,
-        storename: this.orderList[page].storename
+        storename: this.orderList[page].storename,
+        key:this.orderList[page].key,
+        checked:this.orderList[page].checked
       });
     },
     getReturn() {
